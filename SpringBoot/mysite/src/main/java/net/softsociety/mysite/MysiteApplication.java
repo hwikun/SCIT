55e790b26c9de4750e77f0175bffab26fb12ce46
Hwikun <leah8608@gmail.com>
2023-06-26 20:14:57 +0900

    카카오 주소검색api 및 아이디 중복확인

---
+++ SpringBoot/mysite/src/main/java/net/softsociety/mysite/MysiteApplication.java
@@ -0,0 +1,13 @@
+package net.softsociety.mysite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MysiteApplication.class, args);
+	}
+
+}
---
+++ SpringBoot/mysite/src/test/java/net/softsociety/mysite/MysiteApplicationTests.java
@@ -0,0 +1,13 @@
+package net.softsociety.mysite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MysiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
