4006c0fa43050c48aabdeab427b405e404ef0048
Hwikun <leah8608@gmail.com>
2023-07-20 17:53:54 +0900

    20230720 수업내용 (API 통신)

--- SpringBoot/jquery/src/main/java/net/softsociety/jquery/controller/AjaxController.java
+++ SpringBoot/jquery/src/main/java/net/softsociety/jquery/controller/AjaxController.java
@@ -1,11 +1,17 @@
 package net.softsociety.jquery.controller;
 
+import java.util.ArrayList;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import lombok.extern.slf4j.Slf4j;
 import net.softsociety.jquery.domain.Person;
 
@@ -66,4 +72,55 @@ public class AjaxController {
     log.debug("person: {}", p);
   }
 
+  @ResponseBody
+  @PostMapping("insert3")
+  public void insert3(Person p) {
+    log.debug("전달된값 : {}", p);
+  }
+
+  @ResponseBody
+  @GetMapping("getObj")
+  public Person getObj() {
+    Person p = new Person("홍길동", 22, "010-2222-2222");
+    return p;
+  }
+
+  @ResponseBody
+  @GetMapping("getList")
+  public ArrayList<Person> getList() {
+    ArrayList<Person> list = new ArrayList<>();
+    list.add(new Person("홍길동", 22, "010-2222-3333"));
+    list.add(new Person("홍길순", 23, "010-2222-4444"));
+    list.add(new Person("홍길자", 24, "010-2222-5555"));
+    return list;
+  }
+
+  @ResponseBody
+  @PostMapping("sendArr1")
+  public void sendArr1(String []arr) {
+    if(arr == null) log.debug("arr: null");
+    else {
+      for (String s : arr) {
+        log.debug("arr: {}", s);
+      }
+    }
+  }
+
+  @ResponseBody
+  @PostMapping("sendArr2")
+  public void sendArr1(String arr) throws Exception {
+    if (arr == null) {
+      log.debug("arr: null");
+      return;
+    }
+    ObjectMapper objectMapper = new ObjectMapper();
+    ArrayList<Person> list = objectMapper.readValue(arr, new TypeReference<ArrayList<Person>>() {});
+    
+    log.debug("object: {}",list);
+
+    for(Object obj : list) {
+      log.debug("타입: {}",obj.getClass());
+      log.debug("값: {}", obj);
+    }
+  }
 }
